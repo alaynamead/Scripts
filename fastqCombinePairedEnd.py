e50d9c97ae466e2fbd0d599b66bd31ba6f80a588
Alayna Mead <alayna.mead@gmail.com>
2018-12-03 11:13:47 -0800

    Add files via upload
    changes for different read name format, fixes for python 3

--- fastqCombinePairedEnd.py
+++ fastqCombinePairedEnd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.6
 """Resynchronize 2 fastq or fastq.gz files (R1 and R2) after they have been
 trimmed and cleaned
 
@@ -9,18 +9,15 @@ Three output files are generated. The first two files contain the reads of the
     pairs that match and the third contains the solitary reads.
 
 Usage:
-    python fastqCombinePairedEnd.py input1 input2 separator
+    python fastqCombinePairedEnd.py input1 input2
 
 input1 = LEFT  fastq or fastq.gz file (R1)
 input2 = RIGHT fastq or fastq.gz file (R2)
-separator = character that separates the name of the read from the part that
-    describes if it goes on the left or right, usually with characters '1' or
-    '2'.  The separator is often a space, but could be another character. A
-    space is used by default. If the sequence names do not contain two parts
-    and you want to use the full name info to pair your sequences, use 'None'
-    (as text) for the separator. Eg:
-        python fastqCombinePairedEnd.py input1 input2 None
 """
+
+# modified from: https://github.com/enormandeau/Scripts/blob/master/fastqCombinePairedEnd.py
+# by Alayna Mead (28 November 2018)
+# pulls read name as the first 7 fields of name line, assuming they're separated by ":"
 
 # Importing modules
 import gzip
@@ -34,12 +31,12 @@ except:
     print(__doc__)
     sys.exit(1)
 
-try:
-    separator = sys.argv[3]
-    if separator == "None":
-        separator = None
-except:
-    separator = " "
+#try:
+#    separator = sys.argv[3]
+#    if separator == "None":
+#        separator = None
+#except:
+#    separator = " "
 
 # Defining classes
 class Fastq(object):
@@ -52,13 +49,9 @@ class Fastq(object):
         self.name2 = name2
         self.qual = qual
 
-    def getShortname(self, separator):
-        if separator:
-            self.temp = self.name.split(separator)
-            del(self.temp[-1])
-            return separator.join(self.temp)
-        else:
-            return self.name
+    def getShortname(self):
+        temp = self.name.split(':')[0:6]
+        return(':'.join(temp))
 
     def write_to_file(self, handle):
         handle.write(self.name + "\n")
@@ -69,14 +62,11 @@ class Fastq(object):
 # Defining functions
 def myopen(infile, mode="r"):
     if infile.endswith(".gz"):
-        return gzip.open(infile, mode=mode)
+        return gzip.open(infile, mode='rt')
     else:
         return open(infile, mode=mode)
 
 def fastq_parser(infile):
-    """Takes a fastq file infile and returns a fastq object iterator
-    """
-    
     with myopen(infile) as f:
         while True:
             name = f.readline().strip()
@@ -97,41 +87,41 @@ if __name__ == "__main__":
     s1_finished = False
     s2_finished = False
 
-    if in1.endswith('.gz'): 
-    	outSuffix='.fastq.gz'
+    if in1.endswith('fastq.gz'): 
+    	stripChar = 9
     else:
-    	outSuffix='.fastq'
-    	
-    with myopen(in1 + "_pairs_R1" + outSuffix, "w") as out1:
-        with myopen(in2 + "_pairs_R2" + outSuffix, "w") as out2:
-            with myopen(in1 + "_singles" + outSuffix, "w") as out3:
+    	stripChar = 6
+
+    with myopen(in1[:-stripChar] + "_pairs_R1" + '.fastq', "w") as out1:
+        with myopen(in2[:-stripChar] + "_pairs_R2" + '.fastq', "w") as out2:
+            with myopen(in1[:-stripChar] + "_singles" + '.fastq', "w") as out3:
                 while not (s1_finished and s2_finished):
                     try:
-                        s1 = seq1.next()
-                    except:
+                        s1 = next(seq1)
+                    except Exception as e:
                         s1_finished = True
                     try:
-                        s2 = seq2.next()
+                        s2 = next(seq2)
                     except:
                         s2_finished = True
 
                     # Add new sequences to hashes
                     if not s1_finished:
-                        seq1_dict[s1.getShortname(separator)] = s1
+                        seq1_dict[s1.getShortname()] = s1
                     if not s2_finished:
-                        seq2_dict[s2.getShortname(separator)] = s2
+                        seq2_dict[s2.getShortname()] = s2
 
-                    if not s1_finished and s1.getShortname(separator) in seq2_dict:
-                        seq1_dict[s1.getShortname(separator)].write_to_file(out1)
-                        seq1_dict.pop(s1.getShortname(separator))
-                        seq2_dict[s1.getShortname(separator)].write_to_file(out2)
-                        seq2_dict.pop(s1.getShortname(separator))
+                    if not s1_finished and s1.getShortname() in seq2_dict:
+                        seq1_dict[s1.getShortname()].write_to_file(out1)
+                        seq1_dict.pop(s1.getShortname())
+                        seq2_dict[s1.getShortname()].write_to_file(out2)
+                        seq2_dict.pop(s1.getShortname())
 
-                    if not s2_finished and s2.getShortname(separator) in seq1_dict:
-                        seq2_dict[s2.getShortname(separator)].write_to_file(out2)
-                        seq2_dict.pop(s2.getShortname(separator))
-                        seq1_dict[s2.getShortname(separator)].write_to_file(out1)
-                        seq1_dict.pop(s2.getShortname(separator))
+                    if not s2_finished and s2.getShortname() in seq1_dict:
+                        seq2_dict[s2.getShortname()].write_to_file(out2)
+                        seq2_dict.pop(s2.getShortname())
+                        seq1_dict[s2.getShortname()].write_to_file(out1)
+                        seq1_dict.pop(s2.getShortname())
                         
                 # Treat all unpaired reads
                 for r in seq1_dict.values():
